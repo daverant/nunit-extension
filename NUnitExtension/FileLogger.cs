3d83c6a91898e0530b123858f6b01e320efa510e
David Rant <daverant@gmail.com>
2025-03-28 13:47:19 +0100

    init

---
+++ NUnitExtension/FileLogger.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Engine;
+using NUnit.Engine.Extensibility;
+
+namespace NUnitExtension;
+
+[Extension(EngineVersion="3.4")]
+public class FileLogger : ITestEventListener
+{
+    public void OnTestEvent(string report)
+    {
+        File.AppendAllLines($"{nameof(NUnitExtension)}.{nameof(FileLogger)}.txt", [report]);
+    }
+}
---
+++ UnitTests/Tests.cs
@@ -0,0 +1,10 @@
+namespace UnitTests;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
